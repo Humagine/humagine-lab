04beda5d26112174016641fdc5e6761b9239b511
Hugues Larochelle <hlarochelle@pcms.com>
2012-06-27 15:29:31 -0400

    HUMSTW-55: Loader et recadrer des images

--- TestMVC4Website/TestMVC4Website/Services/ImageCropper.cs
+++ TestMVC4Website/TestMVC4Website/Services/ImageCropper.cs
@@ -1,6 +1,7 @@
 namespace TestMVC4Website.Services
 {
 	using System.Drawing;
+	using System.Drawing.Imaging;
 
 	public interface IImageCropper
 	{
@@ -13,9 +14,38 @@ namespace TestMVC4Website.Services
 		{
 			using (var bitmap = new Bitmap(image))
 			{
-				var clone = bitmap.Clone(new RectangleF(x, y, width, height), bitmap.PixelFormat);
-				return clone;
+				using(var copy = Copy(bitmap))
+				{
+					using(var clone = Crop(x, y, width, height, copy, bitmap.PixelFormat))
+					{
+						return Resize(clone);
+					}
+				}
 			}
+		}
+
+		private static Bitmap Copy(Bitmap srcBitmap)
+		{
+			var bmp = new Bitmap(500, 500);
+			bmp.MakeTransparent();
+
+			using(var g = Graphics.FromImage(bmp))
+			{
+				g.DrawImage(srcBitmap, (500 - srcBitmap.Width) / 2, (500 - srcBitmap.Height) / 2);
+			}
+
+			return bmp;
+		}
+
+		private static Bitmap Crop(float x, float y, float width, float height, Bitmap copy, PixelFormat pixelFormat)
+		{
+			var clone = copy.Clone(new RectangleF(x, y, width, height), pixelFormat);
+			return clone;
+		}
+
+		private static Image Resize(Image img)
+		{
+			return new Bitmap(img, new Size(150, 100));
 		}
 	}
 }
